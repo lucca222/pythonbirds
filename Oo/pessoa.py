c34734b40b0bad6001d81e91116cc1dcbcd71090
lucca <luccacccacrcrcr@hotmail.com>
2021-12-30 19:22:26 -0300

    Criado metodo cumprimentar

--- Oo/pessoa.py
+++ Oo/pessoa.py
@@ -1,2 +1,9 @@
 class Pessoa:  # classes são tipos personalizados
-    pass
+    def cumprimentar(self):
+        return f'Olá {id(self)}'
+
+
+if __name__ == '__main__':
+    p = Pessoa()
+    print(Pessoa.cumprimentar(p))
+    print(p.cumprimentar())
