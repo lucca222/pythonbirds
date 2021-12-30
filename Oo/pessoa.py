00aaab7e1b7e5a007d8bf8b5ccd05abc7b300b6a
lucca <luccacccacrcrcr@hotmail.com>
2021-12-30 20:25:38 -0300

    Criado atributos de instancia nome e idade

--- Oo/pessoa.py
+++ Oo/pessoa.py
@@ -1,9 +1,14 @@
 class Pessoa:  # classes são tipos personalizados
-    def cumprimentar(self):
+    def __init__(self, nome=None, idade=35):  # Atributo de dado
+        self.idade = idade
+        self.nome = nome
+
+    def cumprimentar(self):  # metodo
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Lucca')
     print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
+    print(p.nome)
